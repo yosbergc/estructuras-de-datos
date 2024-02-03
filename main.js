a6d60df0a3906e0c06319b93425b9e58973cbc62
Yosber Guerrero <yosbergc@gmail.com>
2024-02-03 16:03:18 -0400

    index added

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
 let startButton = document.querySelector('.start');
+let inicioContainer = document.querySelector('.inicio');
+let index = document.querySelector('.index')
+startButton.addEventListener('click', () => {
+    inicioContainer.classList.add('hide');
+    index.classList.remove('hide');
+})
